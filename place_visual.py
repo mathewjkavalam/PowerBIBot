c9e0d4bb01db67971fabc9172fcc84aa42874a18
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2022-09-26 18:17:50 +0530

    added __name__ condition,imported in Vlang compiler

--- place_visual.py
+++ place_visual.py
@@ -10,9 +10,10 @@ import pyautogui as r
 
 
 debug = False
-
+safe = True
 def function1(h=0,w=0,x=0,y=0,visual="card"): 
-    if debug: r.alert("Start")
+
+    if safe:r.alert("function1")
     r.doubleClick(77,195)
     r.click(1901,193)
     r.click(1671,307)
@@ -24,38 +25,60 @@ def function1(h=0,w=0,x=0,y=0,visual="card"):
     horizontal = str(x)
     vertical = str(y)
 
-    if debug : r.alert("Create Card?")
+
     visual_menu_position = {"card":(1761,480) ,"bar": (1713,355)}
     r.click(visual_menu_position[visual][0],visual_menu_position[visual][1])
-    if debug : r.alert("Properties?")
+
     r.click(1735,277)
-    if debug : r.alert("General?")
+
     r.click(1735,413)
-    if debug : r.alert("Properties?")
+
     r.click(1621,481)
-    if debug : r.alert("Height as:"+height)
+
     r.click(1639,619)
     r.hotkey('ctrl','a')
     r.hotkey('delete')
     r.write(height) 
-    if debug : r.alert("Width as:"+width)
+
     r.click(1643,697)
     r.hotkey('ctrl','a')
     r.hotkey('delete')
     r.write(width)
-    if debug : r.alert("Position")
+
     r.click(1633,811)
-    if debug : r.alert("Horizontal as:"+horizontal)
+
     r.click(1647,891)
     r.hotkey('ctrl','a')
     r.hotkey('delete')
     r.write(horizontal)
-    if debug : r.alert("Vertical as:"+vertical)
+
     r.click(1639,969)
     r.hotkey('ctrl','a')
     r.hotkey('delete')
     r.write(vertical)
 
-r.alert("Start")
-# function1(200,200,50,100,"bar")
-function1(200,200,50,300,"card")
+
+
+if __name__ == "__main__":
+    x=20
+    Y=100
+    width=100
+    height=100
+    gap=10
+    gaap=30
+
+    #Card1
+    print("Card placed at:",x,Y)
+    function1(height,width,x,Y,"card")
+    x+=width+gap
+    #Card2
+    print("Card placed at:",x,Y)
+    function1(height,width,x,Y,"card")
+    #Card3
+    x+=width+gaap
+    print("Card placed at:",x,Y)
+    function1(height,width,x,Y,"card")
+    #Card4
+    x+=width+gap
+    print("Card placed at:",x,Y)
+    function1(height,width,x,Y,"card")
