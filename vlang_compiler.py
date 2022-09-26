85c2e73fa7557ae078c771c9fe7218b9bbe22ed9
Mathew Jacob Kavalam <mathewjkavalam@gmail.com>
2022-09-26 18:46:07 +0530

    Inserted J832

--- vlang_compiler.py
+++ vlang_compiler.py
@@ -18,20 +18,22 @@ while not quit:
     if debug: print(in_word)
     opword = in_word[0:3]
     if opword == '`': quit = True
-    if opword in vlang_opwords and not quit:
-        i += 1
-        args = in_word[3:]
-        if debug:print(in_word)
-        args = args.split(',')
-        if debug:print(args)
-        for each in args:
-            each = each.strip()
-            if debug: print(each)
-            cleaned.append(each)
-        tokens[i] = {"op":opword,"args":cleaned}
-        if debug:tokens
-        if opword == 'CDX':
-            function1(args[0],args[1],args[2],args[3],args[4])
-    else:
-        print("Not a recognised keywords:"+in_word)
+    if not quit:
+        if opword in vlang_opwords:
+            i += 1
+            args = in_word[3:]
+            if debug:print(in_word)
+            args = args.split(',')
+            if debug:print(args)
+            for each in args:
+                each = each.strip()
+                if debug: print(each)
+                cleaned.append(each)
+            tokens[i] = {"op":opword,"args":cleaned}
+            if debug:tokens
+            if opword == 'CDX':
+                function1(args[0],args[1],args[2],args[3],args[4])
+        else:
+            print("Not a recognised keywords:"+in_word)
     if debug: print(tokens)
+    print("remember J832")
